b6f26b4af88c0f5a150db492d59bd178697d17e4
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2019-04-29 16:18:04 -0500

    feat add component signup

--- src/app/invited/invited-routing.module.ts
+++ src/app/invited/invited-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { SignIn } from './signin/signin.component';
 import { InvitedComponent } from './invited.component';
+import { SignUp } from './signup/signup.component';
 
 const routes: Routes = [
   {
     path: '',
     component: InvitedComponent,
     children: [
-      { path: 'sigin', component: SignIn },
+      { path: 'signin', component: SignIn },
+      { path: 'signup', component: SignUp },
       { path: '', redirectTo: '/', pathMatch: 'full' },
       { path: '**', redirectTo: '/' }
     ]
@@ -24,5 +26,6 @@ export class InvitedRoutingModule { }
 
 export const routedComponents = [
   InvitedComponent,
-  SignIn
+  SignIn,
+  SignUp
 ];
---
+++ src/app/invited/signup/signup.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'signUp',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignUp implements OnInit {
+
+  constructor() {}
+  ngOnInit() {}
+
+}
