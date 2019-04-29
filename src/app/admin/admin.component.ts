ade4eae6a1507e8ed4285082e4de059cd69b61ba
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2019-04-29 17:21:31 -0500

    updated footer

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -4,24 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngx-admin',
   styleUrls: ['admin.component.scss'],
-  template: `
-  <div class="main">
-    <header>
-      <app-navbar></app-navbar>
-    </header>
-    <div id='main'>
-      <article>
-        <router-outlet></router-outlet>
-      </article>
-      <nav>
-        <app-sidebar></app-sidebar>
-      </nav>
-      <!--  <aside>
-        <app-aside></app-aside>
-      </aside> -->
-    </div>
-    <footer>footer</footer>
-  </div>
-  `,
+  templateUrl: './admin.component.html',
 })
 export class AdminComponent {}
