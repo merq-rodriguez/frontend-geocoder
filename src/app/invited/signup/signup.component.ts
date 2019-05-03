73ce4547f817b2125a9552f1b131f1ae13d2f838
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2019-05-03 01:11:02 -0500

    validate email

--- src/app/invited/signup/signup.component.ts
+++ src/app/invited/signup/signup.component.ts
@@ -7,7 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUp implements OnInit {
 
+  public names:string = "";
+  public email:string = "";
+  public password:string = "";
+
   constructor() {}
   ngOnInit() {}
+
+  
+  public validate() {
+    if(this.names.trim() === '' || this.email.trim() === '' || this.password.trim() === ''){
+      return false;
+    }
+    return true;
+    
+  }
+
+  public validateEmail(){
+    let patron=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    if(this.email.search(patron) == 0){
+      console.log("Si es un correo");
+      return true;
+    }else{
+      console.log("No es un correo");
+      return false;
+    }
+  }
+
+  signUp(){
+    if(this.validate()){
+      console.log("Enviado usuario al backend");
+    }else{
+      console.log("Campos vacios");
+      
+    }
+  }
 
 }
