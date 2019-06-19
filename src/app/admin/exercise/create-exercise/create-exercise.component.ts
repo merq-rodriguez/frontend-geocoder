b187b692f868112253e08bbfd8274342bb5ada7f
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2019-06-19 05:12:08 -0500

    feat add mat-select for language in exercise component

--- src/app/admin/exercise/create-exercise/create-exercise.component.ts
+++ src/app/admin/exercise/create-exercise/create-exercise.component.ts
@@ -1,5 +1,5 @@
 import { Judge0Service } from './../../../@core/services/judge0.service';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { RouteInfo } from 'src/app/@theme/components/navroutes/navroutes.component';
@@ -15,8 +15,13 @@ import { ExerciseService } from 'src/app/@core/services/exercise.service';
 import { InfoDialogComponent } from '../../modal/info/info-dialog.component';
 import { PayloadJudge0 } from 'src/app/@core/data/payload-judge0';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Coordinate } from 'src/app/@theme/components/map/map.component';
 import { LatLng } from 'leaflet';
+
+
+export interface LanguageOption {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: "create-exercise",
@@ -24,12 +29,29 @@ import { LatLng } from 'leaflet';
   styleUrls: ["./create-exercise.component.css"],
   animations: fuseAnimations
 })
+
+
+
+
+
 export class CreateExerciseComponent implements OnInit {
+
+  selectedLanguage: string;
+
+  
+
+  languages: LanguageOption[] = [
+    { id: 1, name: 'Python' },
+    { id: 2, name: 'C++' },
+    { id: 3, name: 'Javascript' }
+  ];
+
+
   idUser: number;
   subscribeMonaco$: any;
   subscribeEditorHTML$: any;
-  exerciseList:IExercise[] = []
-  token:string = '';
+  exerciseList: IExercise[] = []
+  token: string = '';
 
   isLinear = false;
   firstFormGroup: FormGroup;
@@ -63,52 +85,52 @@ export class CreateExerciseComponent implements OnInit {
     private snackService: SnackBarService,
     private exerciseService: ExerciseService,
     private monacoService: MonacoService,
-    private editorService: ContentEditorService,  
-    private localstorageService: LocalstorageService, 
+    private editorService: ContentEditorService,
+    private localstorageService: LocalstorageService,
     public dialog: MatDialog,
     private judgle0Service: Judge0Service
-    ) {}
+  ) { }
 
-    ngOnInit() {
-      let user = this.localstorageService.getLocalstorage('user');
-      this.idUser = user.idUser;   
-      this.getAllExercises(this.idUser);
-      this.subscribeMonaco$ = this.monacoService.content$.subscribe(content => this.exercise.contentCode = content );
-      this.subscribeEditorHTML$ = this.editorService.content$.subscribe(content => this.exercise.contentEditor = content );
-      this.firstFormGroup = this._formBuilder.group({
-        nameCtrl: ['', Validators.required],
-        descriptionCtrl: ['', Validators.required]
-      });
-      this.secondFormGroup = this._formBuilder.group({
-        
-      });
-      this.threeFormGroup = this._formBuilder.group({
-        inputCtrl: ['', Validators.required],
-        outputCtrl: ['', Validators.required]
-      });
-      this.fourFormGroup = this._formBuilder.group({
-        latitudeCtrl: ['', Validators.required],
-        longitudeCtrl: ['', Validators.required],
-        radiusCtrl: ['', Validators.required]
-      });
-    }
+  ngOnInit() {
+    let user = this.localstorageService.getLocalstorage('user');
+    this.idUser = user.idUser;
+    this.getAllExercises(this.idUser);
+    this.subscribeMonaco$ = this.monacoService.content$.subscribe(content => this.exercise.contentCode = content);
+    this.subscribeEditorHTML$ = this.editorService.content$.subscribe(content => this.exercise.contentEditor = content);
+    this.firstFormGroup = this._formBuilder.group({
+      nameCtrl: ['', Validators.required],
+      descriptionCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
 
-  
+    });
+    this.threeFormGroup = this._formBuilder.group({
+      inputCtrl: ['', Validators.required],
+      outputCtrl: ['', Validators.required]
+    });
+    this.fourFormGroup = this._formBuilder.group({
+      latitudeCtrl: ['', Validators.required],
+      longitudeCtrl: ['', Validators.required],
+      radiusCtrl: ['', Validators.required]
+    });
+  }
 
-  getCoordinate(e : LatLng){
+
+
+  getCoordinate(e: LatLng) {
     console.log(e);
     this.exercise.location.latitude = e.lat;
     this.exercise.location.longitude = e.lng;
   }
 
-    // Da formato de texto a la barra de radio 
-    formatLabel(value: number | null) {
-    
-     // this.exercise.radius = 0;
-      if (!value) {return 0}
-      if (value >= 1000) {return Math.round(value / 1000) + "k"}
-      return value;
-    }
+  // Da formato de texto a la barra de radio 
+  formatLabel(value: number | null) {
+
+    // this.exercise.radius = 0;
+    if (!value) { return 0 }
+    if (value >= 1000) { return Math.round(value / 1000) + "k" }
+    return value;
+  }
 
   openDialog(option: number): void {
     this.submissionCode();
@@ -123,8 +145,8 @@ export class CreateExerciseComponent implements OnInit {
     });
   }
 
- 
-  clearExercise(){
+
+  clearExercise() {
     this.exercise = {
       idExercise: '',
       description: '',
@@ -142,34 +164,34 @@ export class CreateExerciseComponent implements OnInit {
     this.editorService.reset();
     console.log("Ejercicio limpiado");
     console.log(this.exercise);
-    
-    
-    
+
+
+
   }
 
-  getFile(file: File){
+  getFile(file: File) {
     this.exercise.image = file;
   }
 
-  submissionCode(){
+  submissionCode() {
     this.judgle0Service.submission(
-        { 
-          source_code: this.exercise.contentCode,  
-          language_id: 30,
-          expected_output: this.exercise.output          
-        } as PayloadJudge0).subscribe(res => {
-         // console.log(res)
-          if(!res['token']){
-              this.openDialog(3);
-          }else{
-            this.token = res['token'];
-           console.log(this.token); 
-           
-          }
-        });  
+      {
+        source_code: this.exercise.contentCode,
+        language_id: 30,
+        expected_output: this.exercise.output
+      } as PayloadJudge0).subscribe(res => {
+        // console.log(res)
+        if (!res['token']) {
+          this.openDialog(3);
+        } else {
+          this.token = res['token'];
+          console.log(this.token);
+
+        }
+      });
   }
 
-  getDetailSubmission():void{
+  getDetailSubmission(): void {
     console.log("--------TOKEN----------")
     console.log(this.token)
     this.judgle0Service.detailSubmission(this.token).subscribe(res => {
@@ -177,83 +199,83 @@ export class CreateExerciseComponent implements OnInit {
       console.log("--------STATUS----------")
       let status = res['status'];
       console.log(status);
-      if(!status){
+      if (!status) {
         console.log("NO hay status")
         this.openDialog(2);
-      }else{
-        if(status.id == 3 ){
+      } else {
+        if (status.id == 3) {
           console.log("Es aceptado XD")
           this.openDialog(0);
-        }else{
+        } else {
           this.openDialog(1);
         }
       }
     });
   }
 
-  createExercise(exercise: IExercise){
+  createExercise(exercise: IExercise) {
     console.log(exercise)
-     this.exerciseService.createExercise(exercise).subscribe(res => {
+    this.exerciseService.createExercise(exercise).subscribe(res => {
       console.log("Response add exercise")
       console.log(res);
-      
-      if(res['result']){
-        exercise.idExercise   = res['result'].idExercise;
-        exercise.idCode       = res['result'].idCode;
-        exercise.imageSrc     = res['result'].image;
+
+      if (res['result']) {
+        exercise.idExercise = res['result'].idExercise;
+        exercise.idCode = res['result'].idCode;
+        exercise.imageSrc = res['result'].image;
         this.exerciseList.push(exercise);
         this.clearExercise();
         this.snackService.openSnackBar('¡Has agregado un nuevo ejercicio!', 'Aceptar');
         console.log(this.exerciseList)
-      }else{
-        this.snackService.openSnackBar('¡Ocurrio un problema al guardar el ejercicio!', 'Aceptar');        
+      } else {
+        this.snackService.openSnackBar('¡Ocurrio un problema al guardar el ejercicio!', 'Aceptar');
       }
-    }) 
-  }
-
-
-  getAllExercises(idUser: number){
-    this.exerciseService.getExercises(idUser).subscribe(exersices => {
-      console.log(exersices.name);
-      for (const exer of exersices) {
-          console.log(exer);
-          this.exerciseList.push({
-            idExercise: exer.idexercise,
-            name: exer.name,
-            description: exer.description,
-            input: exer.input,
-            output: exer.output,
-            idUser: exer.fkuser,
-            imageSrc: exer.image
-          } as IExercise)
-      }
-    
     })
   }
 
-  addExercise(){
+
+  getAllExercises(idUser: number) {
+    this.exerciseService.getExercises(idUser).subscribe(exersices => {
+      console.log(exersices.name);
+      for (const exer of exersices) {
+        console.log(exer);
+        this.exerciseList.push({
+          idExercise: exer.idexercise,
+          name: exer.name,
+          description: exer.description,
+          input: exer.input,
+          output: exer.output,
+          idUser: exer.fkuser,
+          imageSrc: exer.image
+        } as IExercise)
+      }
+
+    })
+  }
+
+  addExercise() {
     let validate = false;
-    if(this.exercise.contentEditor){
-      
-      if(
-        this.exercise.contentEditor.trim() === '' || 
+    if (this.exercise.contentEditor) {
+
+      if (
+        this.exercise.contentEditor.trim() === '' ||
         this.exercise.name === '' ||
         this.exercise.description === '',
-        this.exercise.output === ''){
+        this.exercise.output === '') {
         validate = false;
-      }else{
+      } else {
         validate = true;
       }
-    }else{
+    } else {
       validate = false;
     }
 
     console.log(this.exercise)
 
-    if(!validate){
+    if (!validate) {
       this.snackService.openSnackBar('Hay campos obligatorios sin completar *', 'De acuerdo');
-    }else{
-      
+    } else {
+
       let exer: IExercise = {
         idExercise: uuid(),
         name: this.exercise.name,
@@ -269,18 +291,18 @@ export class CreateExerciseComponent implements OnInit {
           longitude: this.exercise.location.longitude
         },
         radius: this.exercise.radius
-      } 
+      }
       this.createExercise(exer);
     }
-    
+
   }
-  
-  deleteExercise(idExercise: number){
+
+  deleteExercise(idExercise: number) {
     this.exerciseService.deleteExercise(idExercise).subscribe(res => {
       console.log(res)
-      if(res.status){
-        if(res.status === 'OK'){
-          _.remove(this.exerciseList, (ex) =>  Number(ex.idExercise) === idExercise ); //Eliminamos el ejercicio
+      if (res.status) {
+        if (res.status === 'OK') {
+          _.remove(this.exerciseList, (ex) => Number(ex.idExercise) === idExercise); //Eliminamos el ejercicio
           this.snackService.openSnackBar("Has eliminado un ejercicio", "Aceptar");
           console.log(this.exerciseList);
         }
@@ -288,27 +310,27 @@ export class CreateExerciseComponent implements OnInit {
     });
   }
 
-  getAction(e){
-    let exerci: IExercise = e.item; 
+  getAction(e) {
+    let exerci: IExercise = e.item;
     console.log(exerci)
-    switch(e.action){
+    switch (e.action) {
       case 'delete':
-          this.deleteExercise(Number(exerci.idExercise));
-          
-          break;
+        this.deleteExercise(Number(exerci.idExercise));
+
+        break;
       case 'update':
-          console.log("Actualizando ejercicio... equisde equisde")
+        console.log("Actualizando ejercicio... equisde equisde")
 
         break;
     }
-    
+
   }
 
 
-  
 
 
-  
+
+
   public getRoutesItem() {
     return ROUTES_COMPETENCE;
   }
@@ -322,21 +344,21 @@ const ROUTES_COMPETENCE: RouteInfo[] = [
 const MESSAGES_DIALOG = [
 
   {
-    id: 1, 
+    id: 1,
     title: '¡Compilacion exitosa!',
-    message: '', 
+    message: '',
     image: "http://localhost:3001/uploads/images/fine.png"
   },
   {
-    id: 2, 
+    id: 2,
     title: '¡Compilacion fallida!',
-    message: '', 
+    message: '',
     image: "http://localhost:3001/uploads/images/bad.png"
   },
   {
-    id: 3, 
+    id: 3,
     title: '¡El servidor no responde!',
-    message: '', 
+    message: '',
     image: "http://localhost:3001/uploads/images/alert.png"
   }
 
