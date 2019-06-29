37bd3183ced12509c2350a062f9a045e7b118684
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2019-06-28 22:54:05 -0500

    feat: updated url geoprogram

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   api: { 
-    geoprogram: 'http://localhost:3001/', 
+    geoprogram: 'http://space-innovations.tk:3001/', 
     judge: 'https://api.judge0.com/' 
   },
   production: false
