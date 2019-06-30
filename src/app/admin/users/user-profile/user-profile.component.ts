48c1697d7f3ac4408c87a3d4cea85769bd9ebdb5
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2019-06-30 03:26:15 -0500

    feat update image user profile

--- src/app/admin/users/user-profile/user-profile.component.ts
+++ src/app/admin/users/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/@core/services/user.service';
 import { LocalstorageService } from 'src/app/@core/services/localstorage.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,6 +10,7 @@ import { LocalstorageService } from 'src/app/@core/services/localstorage.service
 })
 export class UserProfileComponent implements OnInit {
   user: any;
+  profile: string = environment.api.geoprogram+"uploads/images/image-user-default.jpg"
   constructor(private userService: UserService, private localstorajeService: LocalstorageService) { }
 
   ngOnInit() {
